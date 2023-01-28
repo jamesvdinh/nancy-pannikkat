76e2d689fbf2411e8cb287ea28393e6443d02328
James Dinh <jamesdinh77@gmail.com>
2023-01-28 14:03:34 -0800

    Added email replace

--- js/general.js
+++ js/general.js
@@ -1,24 +1,4 @@
 document.addEventListener( 'DOMContentLoaded', function() {
-    var splide = new Splide( '.splide', {
-        type   : 'loop',
-        perPage: 1,
-        focus  : 'center',
-        autoWidth: true,
-        autoplay: true,
-        mediaQuery: 'max',
-        breakpoints: {
-            1000: {
-                destroy: true,
-            },
-        },
-        lazyLoad: true,
-        keyboard: true,
-        isNavigation: true,
-        pauseOnFocus: true,
-        speed: 2000,
-        interval: 7000,
-    } );
-    splide.mount();
     window.onload = function() {
         uncheck('menu-btn');
     };
@@ -33,8 +13,8 @@ document.addEventListener( 'DOMContentLoaded', function() {
         }
     }
   
-    const tech_emails = document.querySelectorAll('.email-replace');
-    tech_emails.forEach(entry => {
+    const emails = document.querySelectorAll('.email-replace');
+    emails.forEach(entry => {
         setAttributes(entry, {"href": "mailto:nancypannikkat" + "@" + "gmail.com?subject=Art Purchase Inquiry", "target": "_blank"});
         entry.innerHTML = "nancypannikkat" + "@" + "gmail.com";
     });
